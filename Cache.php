0a5336d772562ae5ac0244cedf867b470b9a502c
samueloliva <samuel.zanferdinioliva@gmail.com>
2021-04-02 16:11:22 -0300

    Creating Cache Class

--- Cache.php
+++ Cache.php
@@ -158,17 +158,3 @@ class Cache
     }
 
 }
-
-$cache = new Cache();
-$cache->put("A", "Cache test A!", 2);
-sleep(4);
-$cache->put("B", "Cache test B!");
-$cache->put("C", "Cache test C!");
-$cache->remove("B");
-print_r($cache->find("A"));
-echo "\n";
-print_r($cache->findAll());
-$cache->removeExpired();
-print_r($cache->findAll());
-$cache->removeAll();
-print_r($cache->findAll());
