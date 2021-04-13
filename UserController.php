ae204e76ece227948e5e39fa4215c0e009db3fc7
samueloliva <samuel.zanferdinioliva@gmail.com>
2021-04-13 13:10:51 -0300

    Shortening UserController methods

--- UserController.php
+++ UserController.php
@@ -1,44 +1,51 @@
 <? 
 
 /**
- *  The code provides a very simple implementation for updating Users.
- *  Hence, some improvements can be made, such as validate the request
- *  data, pass an User object, use input instead of get, put comments 
- *  on the code, and check if the use is active through a boolean field
- *  Bellow it follows the code refactored 
- *  */ 
-
+ * A way of creating short methods for UserController would be 
+ * to set routes and using oauth and middleware. So, just admin 
+ * users have access to these methods (update and changeStatus).
+ * 
+ * The validation can be done through the class UserRegisterRequest
+ * instead of doing in the UserController.
+ * 
+ * And I would break the update details and change status in
+ * two methods, one called update and the other changeStatus.
+ * This would turn the methods shorter, clearer and more readable. 
+ */
 
 class UserController
-{
+{   
     /**
-     * Update User
+     * Update User details
      * 
-     * @param User $user
+     * @param $id
+     * @param UserRegisterRequest $request 
+     * @return Response
+     * 
+     */
+    public function update($id, UserRegisterRequest $request ) 
+    {
+        $user = User::find($id);
+        $user->first_name   = $request->input('first_name');
+        $user->last_name    = $request->input('last_name');
+        $user->address      = $request->input('address');
+        $user->save();
+        return redirect()->back()->with('success', 'User updated successfully');
+    }
+
+    /**
+     * Update User status
+     * 
+     * @param $id
      * @param Request $request 
      * @return Response
      * 
      */
-	public function update(User $user, Request $request)
-	{
-        if ($user->isAdmin()) {
-            $this->validate($request, [
-                'first_name'    => 'required',
-                'last_name'     => 'required',
-                'address'       => 'required',
-                'active'        => 'required'
-            ]);
-
-            $user->first_name   = $request->input('first_name');
-            $user->last_name    = $request->input('last_name');
-            $user->address      = $request->input('address');
-            $user->active       = $request->input('active');
-            $user->save();
-
-            return redirect()->back()->with('success', 'User updated successfully');
-        }
-        else {
-            return redirect()->back()->with('fail', 'You do not have access');
-        }
-	}
+    public function changeStatus($id, Request $request) 
+    {
+        $user = User::find($id);
+        $user->active = $request->input('active');
+        $user->save();
+        return redirect()->back()->with('success', 'User updated successfully');
+    }
 }
